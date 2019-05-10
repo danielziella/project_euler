27dc24bdd7396dce2ffb4a0d27fb68c3a9540989
Daniel Horacio Ziella <danielziella@gmail.com>
2019-05-10 17:01:21 -0300

    Solution to problem 9

--- project_euler/solutions.py
+++ project_euler/solutions.py
@@ -130,3 +130,19 @@ def problem8():
 
     return max([functools.reduce(operator.mul, map(int, astring[k:k+13]))
                 for k in range(len(astring))])
+
+
+def problem9():
+    """A Pythagorean triplet is a set of three natural numbers, a < b < c, for
+    which,
+        a^2 + b^2 = c^2
+
+    For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    Find the product abc."""
+
+    for a in range(1, 1000):
+        for b in range(1, a+1):
+            if a**2 + b**2 == (1000 - a - b)**2:
+                return a*b*(1000 - a - b)
